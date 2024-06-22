007b6e130dd8a326537b0d519680097ba082a20b
Paras Naulia <parasnaulia645@gmail.com>
2024-06-22 18:02:02 +0530

    first commit11

--- src/Db/Conn.js
+++ src/Db/Conn.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-async function Connection() {
+
+(async function connectToDatabase() {
   try {
-    const conn = await mongoose.connect(
+    await mongoose.connect(
       "mongodb+srv://parasnaulia645:nx7w2cQ4pHnjfTbl@cluster0.ivdbulp.mongodb.net/parasnaulia645"
     );
-    console.log("Connection is Established");
+    console.log("Connection is successful");
   } catch (e) {
-    console.log(e + "No Connection");
+    console.log("Database Connection error: " + e);
   }
-}
-module.exports = Connection;
+})();
